c638c9be62ee36b4b74fb5b90a954ab782ae5fc3
adatta4 <adatta4@ncsu.edu>
2022-09-19 12:17:06 -0400

    angular integrated with electron.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { StartStopSessionComponent } from './components/start-stop-session/start-stop-session.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    StartStopSessionComponent
   ],
   imports: [
     BrowserModule,
