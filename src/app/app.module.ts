30736a2ddc19102ebaa0eb1e836668dee6a719f7
adatta4 <adatta4@ncsu.edu>
2022-09-20 18:12:39 -0400

    Material Angular added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { StartStopSessionComponent } from './components/start-stop-session/start-stop-session.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { StartStopSessionComponent } from './components/start-stop-session/start
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
