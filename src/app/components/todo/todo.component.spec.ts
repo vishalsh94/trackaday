909b530b7bef196d529915da1ab57440edcf73a4
adatta4 <adatta4@ncsu.edu>
2022-10-07 21:16:52 -0400

    Fixed some ng test errors

--- src/app/components/todo/todo.component.spec.ts
+++ src/app/components/todo/todo.component.spec.ts
@@ -2,14 +2,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoComponent } from './todo.component';
 
+import { ToastrModule } from 'ngx-toastr';
+
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
-    })
+      declarations: [ TodoComponent ],
+      imports: [
+        ToastrModule.forRoot() // added this works for me
+    ]})
     .compileComponents();
 
     fixture = TestBed.createComponent(TodoComponent);
