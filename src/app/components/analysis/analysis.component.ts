08a27a5ecd59cf23d7b0bb0447ca450f7ac7cdbf
Atharva Gole <40511985+AtharvaGole@users.noreply.github.com>
2022-10-10 00:14:55 -0400

    Update analysis.component.ts

--- src/app/components/analysis/analysis.component.ts
+++ src/app/components/analysis/analysis.component.ts
@@ -1,6 +1,8 @@
 import { OnInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Chart, Point, registerables } from "chart.js";
+import { DataKey } from 'src/app/models/dataKey';
 import { getTaskTrackingStats, getTimeTrackingStats } from 'src/app/stats/stats';
+const electron = (<any>window).require('electron');
 
 Chart.register(...registerables);
 
@@ -12,25 +14,42 @@ Chart.register(...registerables);
 })
 
 export class AnalysisComponent implements OnInit {
-  // @ViewChild('chart') chart: Chart;
-  // private chart: Chart;
   private data: Point[];
+  private result: any;
+
 
   constructor() {
     this.data = [{ x: 1, y: 5 }, { x: 2, y: 10 }, { x: 3, y: 6 }, { x: 4, y: 2 }, { x: 4.1, y: 6 }];
   }
 
   ngOnInit(): void {
-    this.createTimeTrackigStatsChart();
-    this.createTaskTrackingList();
+    this.readAppData(DataKey.ALL_KEY);
+  }
+
+  readAppData(key: DataKey) {
+    // this.isReading = true;
+    console.log("trying to read data")
+    return new Promise(resolve => {
+      electron.ipcRenderer.send('read-data', key)
+
+      electron.ipcRenderer.once('read-data-reply', (event: any, result: any) => {
+        // resolve(result);
+        // this.readCallback(key, result);
+        // console.log(result);
+        // eval("document.getElementById('abcd').innerHTML = JSON.stringify(" + JSON.stringify(result) + ");");
+
+        this.result = result;
+
+        this.createTimeTrackigStatsChart();
+        this.createTaskTrackingList();
+        this.createHourlyTrackingList();
+      })
+    })
   }
 
   createTimeTrackigStatsChart() {
-    let  stats = getTimeTrackingStats();
+    let stats = getTimeTrackingStats(this.result);
     const timeTrackingStats = stats[0];
-    // for (var i = 0; i < 5; i++) {
-    //   timeTrackingStats = timeTrackingStats.concat(timeTrackingStats);
-    // }
 
     const labelsList = timeTrackingStats.map((val: any[]) => val[0]);
 
@@ -65,23 +84,18 @@ export class AnalysisComponent implements OnInit {
           borderColor: Array(labelsList.length).fill(0).map((_, index) => borderColorList[index % borderColorList.length]),
           borderWidth: 1
         }]
-      },
-      // options: {
-      //   scales: {
-      //     y: {
-      //       beginAtZero: true
-      //     }
-      //   }
-      // }
+      }
     });
 
-    console.log(myChart);
+  }
+
+  callAbc() {
   }
 
   createTaskTrackingList() {
-    const taskTrackingStatsList = getTaskTrackingStats();
-    console.log(taskTrackingStatsList, "taskTrackingStatsList");
+    const taskTrackingStatsList = getTaskTrackingStats(this.result);
 
+    // Implementation for collapsible task list
     let taskTrackingElement = "";
     Object.keys(taskTrackingStatsList).map(day => {
       let taskUL = "<ul>";
@@ -89,12 +103,57 @@ export class AnalysisComponent implements OnInit {
         taskUL += "<li>Task: " + val[0] + " Time: " + Math.round(val[1] / (36 * (10 ** 5)) * 100) / 100 + " hrs</li>"
       });
       taskUL += "</li>";
-      taskTrackingElement += "<li>" + day + taskUL + "</li>"
+      const k = "\"collapsible-body-" + day + "\"";
+      const func = "(()=>{if(document.getElementById(" + k + ").style.display==\"\") {document.getElementById(" + k + ").style.display=\"block\"} else {document.getElementById(" + k + ").style.display=\"\"}})()";
+      taskTrackingElement += "<li><div class='collapsible-header' onClick='" + func + "'>" + day + "</div><div class='collapsible-body' id='collapsible-body-" + day + "'>" + taskUL + "</div></li>"
     });
 
     const el = document.getElementById("taskTrackingStats");
     if (el !== null) {
       el.innerHTML = taskTrackingElement;
     }
+  }
+
+  createHourlyTrackingList() {
+    let stats = getTimeTrackingStats(this.result);
+    const timeTrackingStats = stats[1];
+
+    const labelsList = Array(24).fill(0).map((_, index) => index);
+
+    const dataList = labelsList.map(val => timeTrackingStats[val]);
+
+    const backgroundColorList = [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)'
+    ];
+
+    const borderColorList = [
+      'rgba(255, 99, 132, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)'
+    ];
+
+    const myChart = new Chart("hourlyTrackingStats", {
+      type: 'bar',
+      data: {
+        labels: labelsList,
+        datasets: [{
+          label: 'Average user involvment during the day',
+          data: dataList,
+          backgroundColor: Array(labelsList.length).fill(0).map((_, index) => backgroundColorList[index % backgroundColorList.length]),
+          borderColor: Array(labelsList.length).fill(0).map((_, index) => borderColorList[index % borderColorList.length]),
+          borderWidth: 1
+        }]
+      }
+    });
+
+
   }
 }
